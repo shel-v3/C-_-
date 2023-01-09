b3379b9eeec7a45937d46ac044a29f7424391372
shel-v3 <90749168+shel-v3@users.noreply.github.com>
2023-01-09 21:02:48 +0200

    laba4

---
+++ laba4.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labWork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 4;
+            int[] array;
+            array = new int[N];
+            array[0] = 1;
+            array[1] = 3;
+            array[2] = 2;
+            array[3] = 1;
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+            Console.WriteLine();
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+        }
+    }
+}
