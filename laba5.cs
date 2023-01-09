7288652f99f6f19e9c21d35e1cd8d6d24d82d46a
shel-v3 <90749168+shel-v3@users.noreply.github.com>
2023-01-09 21:03:11 +0200

    laba5

---
+++ laba5.cs
@@ -0,0 +1,33 @@
+﻿int a = 3;
+int b = 3;
+int[,] m = new int[a, b];
+int min = int.MaxValue;
+int max = int.MinValue;
+
+m[0, 0] = 2;
+m[0, 1] = 7;
+m[0, 2] = 5;
+m[1, 0] = 4;
+m[1, 1] = 7;
+m[1, 2] = 8;
+m[2, 0] = 0;
+m[2, 1] = 21;
+m[2, 2] = 3;
+
+for (int i = 0; i < a; i++)
+{
+    for (int j = 0; j < b; j++)
+    {
+        if (min > m[i, j])
+        {
+            min = m[i, j];
+        }
+        if (max < m[i, j])
+        {
+            max = m[i, j];
+        }
+    }
+}
+Console.WriteLine("min=" + min);
+Console.WriteLine();
+Console.WriteLine("max=" + max);
