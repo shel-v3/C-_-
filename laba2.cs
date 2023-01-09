ae92746776a4326da18c9f3b0d717175f848df34
shel-v3 <90749168+shel-v3@users.noreply.github.com>
2023-01-09 20:43:18 +0200

    laba2

---
+++ laba2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn;
+            int nk;
+            int k;
+            double r = 0;
+
+            do
+            {
+                Console.WriteLine("Write nn");
+                nn = int.Parse(Console.ReadLine());
+            }
+            while (nn <= 0);
+
+            do
+            {
+                Console.WriteLine("Write nk");
+                nk = int.Parse(Console.ReadLine());
+            }
+            while (nk <= nn);
+
+            for (k = nn; k < nk; k++)
+            {
+                r += Math.Pow(k, 2) - 3 / Math.Pow(-1, k) * Math.Pow(k, 2) + 5;
+            }
+
+            Console.WriteLine(r);
+        }
+
+    }
+}
