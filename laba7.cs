aa124d1a4d1493afafc206a6476fde8d71b42779
shel-v3 <90749168+shel-v3@users.noreply.github.com>
2023-01-09 21:06:11 +0200

    laba7

---
+++ laba7.cs
@@ -0,0 +1,68 @@
+﻿namespace LabWork7
+{
+    public static class Program
+    {
+        private static List<int> L;
+        private static void Main()
+        {
+            L = new List<int>();
+            List<int> NL = new List<int>
+            {
+                7,
+                4,
+                7,
+                7,
+                4,
+                7,
+                12,
+                12,
+                12
+            };
+            Duplicates(NL);
+
+            int[] A = Array.Empty<int>();
+            LA(L, A);
+        }
+
+        private static void Duplicates(List<int> IN)
+        {
+            int[] d = IN.Distinct().ToArray();
+            for (int i = 0; i < d.Length; i++)
+            {
+                L.Add(0);
+            }
+            for (int i = 0; i < d.Length; i++)
+            {
+                for (int j = 0; j < IN.Count; j++)
+                {
+                    if (IN[j] == d[i])
+                    {
+                        L[i]++;
+                    }
+                }
+            }
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.WriteLine(d[i] + "=" + L[i]);
+            }
+        }
+
+        private static void LA(List<int> IL, int[] IA)
+        {
+            IA = new int[IL.Count];
+            int i = 0;
+
+            for (int j = 0; j < IL.Count; j++)
+            {
+                int intNumber = IL[j];
+                IA[i++] = intNumber;
+            }
+
+            for (int j = 0; j < IA.Length; j++)
+            {
+                int iN = IA[j];
+                Console.WriteLine(iN);
+            }
+        }
+    }
+}
