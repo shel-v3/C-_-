b2d4d9e21455a15cff1f802136d422065daa1255
shel-v3 <90749168+shel-v3@users.noreply.github.com>
2023-01-09 20:32:59 +0200

    laba1

---
+++ laba1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double P = 3.14f;
+            double g = 9.8f;
+            double l;
+            double T;
+            Console.WriteLine("Write l");
+            l = double.Parse(Console.ReadLine());
+
+            T = 2 * P * Math.Sqrt(l / g);
+            Console.WriteLine("T=" + T);
+        }
+    }
+}
